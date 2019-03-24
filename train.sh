13af18094958236e6ca34f0af4052df9f7f1e6ba
PlayDeep <zxqchat@hotmail.com>
2019-03-24 09:29:30 +0800

    add gpu params

--- train.sh
+++ train.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 data=/data1/xqzhou/new_thumt_Dir/data_nobpe
-python3 train.py -s $data/gq.chn.tmp.shuf -t $data/gq.eng.tmp.shuf --validation-source $data/nist06.in --validation-target $data/nist06.ref.0.low --output ./res
+python3 train.py --device-ids 3 -s $data/gq.chn.tmp.shuf -t $data/gq.eng.tmp.shuf --validation-source $data/nist06.in --validation-target $data/nist06.ref.0.low --output ./res
